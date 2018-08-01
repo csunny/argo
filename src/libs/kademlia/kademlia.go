2b24d22624e30866f7d5a0d83eefef722763ebfa
csunny <cfqcsunny@gmail.com>
2018-08-01 21:39:36 +0800

    DHT 分布式hash表

--- src/libs/kademlia/kademlia.go
+++ src/libs/kademlia/kademlia.go
@@ -1,16 +1,132 @@
 package kademlia
 
 import (
-	"container/heap"
+	"net/rpc"
+	"net"
+	"net/http"
+	"fmt"
+	"log"
 )
 
 type Kademlia struct {
-	routers *RoutingTable
+	routers   *RoutingTable
 	NetworkId string
 }
 
-func NewKademlia(contract *Contract, networkId string) (ret *Kademlia)  {
+type KademliaCore struct {
+	kad *Kademlia
+}
+
+type RPCHeader struct {
+	Sender    *Contract
+	networkId string
+}
+type PingRequest struct {
+	RPCHeader
+}
+
+type PingResponce struct {
+	RPCHeader
+}
+
+type FindNodeRequest struct {
+	RPCHeader
+	target NodeID
+}
+
+type FindNodeResponse struct {
+	RPCHeader
+	contacts []Contract
+}
+
+func NewKademlia(contract *Contract, networkId string) (ret *Kademlia) {
 	ret = new(Kademlia)
+	ret.routers = NewRoutingTable(contract)
+	ret.NetworkId = networkId
 	return
 }
 
+func (k *Kademlia) Serve() error {
+	rpc.Register(&KademliaCore{k})
+
+	rpc.HandleHTTP()
+
+	l, err := net.Listen("tcp", k.routers.node.address)
+
+	if err != nil {
+		return err
+	}
+	go http.Serve(l, nil)
+	return nil
+}
+
+func (k *Kademlia) Call(contract *Contract, method string, args, reply interface{}) error {
+	client, err := rpc.DialHTTP("tcp", contract.address)
+	if err != nil {
+		return err
+	}
+
+	err = client.Call(method, args, reply)
+	if err != nil {
+		return nil
+	}
+
+	k.routers.Update(contract)
+	return nil
+}
+
+func (k *Kademlia) sendQuery(node *Contract, target NodeID, done chan []Contract) {
+	args := FindNodeRequest{RPCHeader{&k.routers.node, k.NetworkId}, target}
+	reply := FindNodeResponse{}
+
+	err := k.Call(node, "KademliaCore.FindNode", &args, &reply)
+	if err != nil {
+		done <- []Contract{}
+	}
+
+	done <- reply.contacts
+
+}
+
+func (k *Kademlia) InterativeFindNode(target NodeID, delta int) []Contract {
+	return []Contract{}
+}
+
+func (k *Kademlia) HandleRPC(request, response *RPCHeader) error {
+	if request.networkId != k.NetworkId {
+		return fmt.Errorf("---")
+	}
+
+	if request.Sender != nil {
+		k.routers.Update(request.Sender)
+	}
+
+	response.Sender = &k.routers.node
+	return nil
+}
+
+func (kc *KademliaCore) Ping(args *PingRequest, response *PingResponce) error {
+	err := kc.kad.HandleRPC(&args.RPCHeader, &response.RPCHeader)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("ping from %s", args.RPCHeader)
+	return nil
+}
+
+func (kc *KademliaCore) FindNode(args *FindNodeRequest, response *FindNodeResponse) error {
+	err := kc.kad.HandleRPC(&args.RPCHeader, &response.RPCHeader)
+
+	if err != nil {
+		return err
+	}
+	contancts := kc.kad.routers.FindClosest(args.target, BucketSize)
+	response.contacts = make([]Contract, len(contancts))
+
+	for i := 0; i < len(contancts); i++ {
+		response.contacts[i] = contancts[i]
+	}
+
+	return nil
+}
--- src/libs/kademlia/routingtable.go
+++ src/libs/kademlia/routingtable.go
@@ -1,42 +1,77 @@
 package kademlia
 
-import (
-	"container/list"
-	"github.com/csunny/argo/src/iterator"
-
-)
-
 const BucketSize = 20
 
 type RoutingTable struct {
 	node    Contract
-	buckets [IdLength * 8]*list.List
+	buckets [IdLength * 8]Contract
 }
 
 type ContractRecord struct {
-	node *Contract
+	node    *Contract
 	sortKey NodeID
 }
 
-func NewRoutingTable(node *Contract) (ret *RoutingTable)  {
+func (rec *ContractRecord) Less(other interface{}) bool {
+	return rec.sortKey.Less(other.(*ContractRecord).sortKey)
+}
+
+func NewRoutingTable(node *Contract) (ret *RoutingTable) {
 	ret = new(RoutingTable)
-	for i:=0; i<IdLength * 8; i++{
-		ret.buckets[i] = list.New()
+	for i := 0; i < IdLength*8; i++ {
+		ret.buckets[i] = *new(Contract)
 	}
 	ret.node = *node
 	return
 }
 
-func (table *RoutingTable) Update(contract *Contract)  {
+func (table *RoutingTable) Update(contract *Contract) {
 	prefix_length := contract.id.Xor(table.node.id).PrefixLen()
-	bucket := table.buckets[prefix_length]
+	bucket := table.buckets[:prefix_length]
 
-	//element := iterable.Find(bucket, func(x interface{}) bool {
-	//	return x.(*Contract).id.Equals(table.node.id)
-	//})
+	var element interface{}
+
+	for _, b := range bucket {
+		if Equal(b, table) == true {
+			element = b
+			break
+		}
+	}
+
+	if element == nil {
+		if len(bucket) < BucketSize {
+			bucket = append(bucket, *contract)
+		}
+	}
 
 }
 
-//func copyToVector(start, end *list.Element, vec *)  {
-//
-//}
+func Equal(x interface{}, table *RoutingTable) bool {
+	return x.(*Contract).id.Equals(table.node.id)
+}
+
+func (table *RoutingTable) FindClosest(target NodeID, count int) []Contract {
+
+	ret := []Contract{}
+
+	bucket_num := target.Xor(table.node.id).PrefixLen()
+
+	bucket := table.buckets[bucket_num]
+	ret = append(ret, bucket)
+	for i := 1; (bucket_num-i >= 0 || bucket_num+i < IdLength*8) && len(ret) < count; i++ {
+		if bucket_num-i >= 0 {
+			bucket = table.buckets[bucket_num-i]
+			ret = append(ret, bucket)
+		}
+		if bucket_num+i < IdLength*8 {
+			bucket = table.buckets[bucket_num+i]
+			ret = append(ret, bucket)
+		}
+	}
+
+	if len(ret) > count{
+		ret = ret[:count]
+	}
+	return ret
+
+}
